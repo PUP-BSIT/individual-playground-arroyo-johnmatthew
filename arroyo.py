99dd9939c9d460c737f7bcffd8b6f7c31ca614fa
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2024-12-08 21:05:07 +0800

    I made comment in arroyo.py

--- arroyo.py
+++ arroyo.py
@@ -1,5 +1,5 @@
 def arroyo_menu():
-    print("1.Greetings, Jjohn Matthew Arroyo")
+    print("1.Greetings, Jjohn Matthew Arroyo") #I made a comment james michael
     print("2.Basic Info: I am an 18 years old DIT student in PUP Taguig who loves tech-related stuff")
     print("3.Goals: Improve my IT skills especially programming and develop myself physically and spiritually")
     #Grammar error -Lazaro
