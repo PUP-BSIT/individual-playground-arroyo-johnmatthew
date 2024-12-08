e2efca5811d74970d2f01242ccfb26b2ad27bfd5
John Matthew Saraga Arroyo <johnmatthewarroyo2@gmail.com>
2024-12-08 15:22:15 +0800

    create module(arroyo)

---
+++ arroyo.py
@@ -0,0 +1,4 @@
+def arroyo_menu():
+    print("Greetings, John Matthew Arroyo")
+    print("Basic Info: I am an 18 years old DIT student in PUP Taguig who loves tech-related stuff")
+    print("Goals: Improve my IT skills especially programming and develop myself physically and spiritually")
