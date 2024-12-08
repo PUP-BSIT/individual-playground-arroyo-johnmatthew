71959905e028c07a20c0298e33b16173cf208d9a
John Matthew Saraga Arroyo <johnmatthewarroyo2@gmail.com>
2024-12-08 21:01:42 +0800

    modify code

--- arroyo.py
+++ arroyo.py
@@ -1,4 +1,8 @@
 def arroyo_menu():
-    print("1.Greetings, John Matthew Arroyo")
+    print("1.Greetings, Jjohn Matthew Arroyo")
     print("2.Basic Info: I am an 18 years old DIT student in PUP Taguig who loves tech-related stuff")
     print("3.Goals: Improve my IT skills especially programming and develop myself physically and spiritually")
+    #Grammar error -Lazaro
+    #Too short -Nolluda
+
+    #KAIN LNG AKO AFK BOI
---
+++ main.py
@@ -0,0 +1,5 @@
+from arroyo import arroyo_menu
+user_input = int(input("Enter choice: "))
+
+if user_input == 1:
+    arroyo_menu()
