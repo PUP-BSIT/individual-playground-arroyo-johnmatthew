0d002c35572700f940bf2f564e5c849daaa21c62
John Matthew Saraga Arroyo <johnmatthewarroyo2@gmail.com>
2024-12-09 13:31:07 +0800

    add no code in feature-2

--- arroyo.py
+++ arroyo.py
@@ -1,8 +1,1 @@
-def arroyo_menu():
-    print("1.Greetings, Jjohn Matthew Arroyo") #I made a comment james michael
-    print("2.Basic Info: I am an 18 years old DIT student in PUP Taguig who loves tech-related stuff")
-    print("3.Goals: Improve my IT skills especially programming and develop myself physically and spiritually")
-    #Grammar error -Lazaro
-    #Too short -Nolluda
-
-    #KAIN LNG AKO AFK BOI
+#This is my change. NO CODES
